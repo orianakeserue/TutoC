5836a903d44d9e2296a591c34eb78b8510638a30
Oriana Keserue <o.keseruepittet@gmail.com>
2026-05-10 18:42:45 +0200

    "Adding files from the first "écrivez votre premier programme" course of openclassrooms"

---
+++ main.c
@@ -0,0 +1,23 @@
+// 
+// main.c
+// TutoC
+//
+// Created by Oriana Keserue on 10/05/2026
+//
+
+#include <stdio.h> //ce sont des bibliothèques et permettent d'afficher du texte à l'écran
+#include <stdlib.h> // cette librairy sera pas permise à la piscine
+
+int main(int argc, const char * argv[]) { //fonction nommée "main" (principale)
+    // insert code here...
+    printf("Bonjour\n"); //instructions de la fonction, finit tjrs par ;
+    return 0; // indique la fin de la fonction main et de rendre valeur 0 => indique que tout s'est bien passé
+}
+
+/* C'est le code minimal en C dont on a besoin
+peut aussi s'écrire plus simplement:
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+} */
